f45f9fade072728bfa8cfd9105cf67616a2bea2b
codenoobies <dev.justcallmesu@gmail.com>
2025-07-28 15:12:25 +0700

    Fix (Middleware): Update response handling in AuthMiddleware to use the correct response package

--- internal/api/middleware.go
+++ internal/api/middleware.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"justcallmesu.com/rest-api/internal/api/cookies"
+	"justcallmesu.com/rest-api/internal/api/response"
 	"justcallmesu.com/rest-api/internal/app/auth"
 )
 
@@ -14,7 +15,7 @@ func AuthMiddleware() gin.HandlerFunc {
 		tokenString, tokenError := context.Cookie(os.Getenv("COOKIE_NAME"))
 
 		if tokenError != nil {
-			context.JSON(http.StatusUnauthorized, NewResponse("Not Authorized, Please Login", false, nil))
+			context.JSON(http.StatusUnauthorized, response.NewResponse("Not Authorized, Please Login", false, nil))
 			context.Abort()
 			return
 		}
@@ -22,8 +23,13 @@ func AuthMiddleware() gin.HandlerFunc {
 		claims, claimsError := auth.ParseToken(tokenString)
 
 		if claimsError != nil {
-			context.JSON(http.StatusUnauthorized, NewResponse(claimsError.Error(), false, nil))
-			cookies.SetCookie(context, os.Getenv("COOOKIE_NAME"), "", -1)
+			context.JSON(http.StatusUnauthorized, response.NewResponse(claimsError.Error(), false, nil))
+			setCookieError := cookies.SetCookie(context, os.Getenv("COOOKIE_NAME"), "", -1)
+
+			if setCookieError != nil {
+				context.JSON(http.StatusUnauthorized, response.NewResponse(setCookieError.Error(), false, nil))
+			}
+
 			context.Abort()
 			return
 		}
