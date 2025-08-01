dc56f9bda3a73fa952e9f25b47c9016e4bb41062
codenoobies <dev.justcallmesu@gmail.com>
2025-08-01 17:22:13 +0700

    refactor (auth): replace Email field with Username in LoginUser struct for consistency

--- internal/app/auth/auth.go
+++ internal/app/auth/auth.go
@@ -1,6 +1,6 @@
 package auth
 
 type LoginUser struct {
-	Email    string `json:"email" binding:"required"`
+	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
