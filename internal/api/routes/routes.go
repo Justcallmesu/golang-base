c730524a88e241b52fa6176154c5b94c100e61c4
codenoobies <dev.justcallmesu@gmail.com>
2025-08-12 13:30:21 +0700

    feat(routes): refactor SetupRoutes to integrate services and middlewares for improved route handling

--- internal/api/routes/routes.go
+++ internal/api/routes/routes.go
@@ -3,11 +3,16 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
-	"justcallmesu.com/rest-api/internal/api"
+	"justcallmesu.com/rest-api/internal/app"
 )
 
 func SetupRoutes(ginEngine *gin.Engine, database *gorm.DB) {
 
-	AuthRoutes(ginEngine, database)
-	ginEngine.Use(api.AuthMiddleware())
+	var repositories = app.NewRepositories(database)
+	var services = app.NewServices(repositories)
+
+	var middlewares = app.NewMiddlewares(services)
+
+	AuthRoutes(ginEngine, services,middlewares)
+	ginEngine.Use(middlewares.AuthMiddleware.EnsureSessionIsValid())
 }
