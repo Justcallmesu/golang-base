cca6bafa995172dcb7ba183b0afaf369f2e035ae
codenoobies <dev.justcallmesu@gmail.com>
2025-08-12 13:30:41 +0700

    feat(routes): refactor AuthRoutes to utilize services and middlewares for improved authentication handling

--- internal/api/routes/auth.go
+++ internal/api/routes/auth.go
@@ -2,18 +2,18 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
+	"justcallmesu.com/rest-api/internal/app"
 	"justcallmesu.com/rest-api/internal/app/auth"
 )
 
-func AuthRoutes(engine *gin.Engine, database *gorm.DB) {
+func AuthRoutes(engine *gin.Engine, services *app.Services, middlewares *app.Middlewares) {
 
 	authRouter := engine.Group("/auth")
 
 	//Handler
-	authHandler := auth.NewAuthHandler(database)
+	authHandler := auth.NewAuthHandler(services.AuthService, services.CookieService)
 
 	// Routes
 	authRouter.POST("/login", authHandler.Login)
-	authRouter.GET("/logout", authHandler.Logout)
+	authRouter.GET("/logout", middlewares.AuthMiddleware.EnsureSessionIsValid(), authHandler.Logout)
 }
