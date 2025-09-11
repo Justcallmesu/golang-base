02b9f8ca8f3fb235bacc5a0ecca5f7b8b957d7c1
codenoobies <dev.justcallmesu@gmail.com>
2025-09-11 20:50:04 +0700

    feat(seeder): enhance user seeding logic with error handling and password hashing

--- cmd/seeder/main.go
+++ cmd/seeder/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"context"
+	"os/user"
+
+	"gorm.io/gorm"
 	"justcallmesu.com/rest-api/internal/app/users"
 	"justcallmesu.com/rest-api/internal/config"
 	"justcallmesu.com/rest-api/internal/database"
@@ -9,18 +13,35 @@ import (
 func main() {
 
 	config.LoadConfig()
+	ctx := context.Background()
 
 	database := database.InitConnection()
-
+	
 	users := []*users.User{
 		{
-			Username: "",
-			Password: "",
+			Model: gorm.Model{
+				ID:1,
+			},
+			Username: "justcallmesu",
+			Password: "portfolioAppPasswordForJustcallmesu12361239789$!@#!@#!@*#@!)!",
 		},
 	}
 
+	
 	for _, value := range users {
 		hashError := value.HashPassword()
+		_,deleteError := gorm.G[user.User](database).Where("id = ?", value.ID).Delete(ctx)
+
+		if(deleteError != nil){
+
+			switch deleteError.Error() {
+			case gorm.ErrRecordNotFound.Error():
+				// Record not found, nothing to delete
+			default:
+				panic("Error deleting existing user: " + deleteError.Error())
+			}
+
+		}
 
 		if hashError != nil {
 			panic("Error hashing password: " + hashError.Error())
