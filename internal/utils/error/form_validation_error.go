ceb18f1a8572a376b653c1e095b729d8be115fd3
CodeNoobies <dev.justcallmesu@gmail.com>
2025-10-06 18:01:25 +0700

    refactor(error): change FormatValidationError return type to any and improve error handling

--- internal/utils/error/form_validation_error.go
+++ internal/utils/error/form_validation_error.go
@@ -2,16 +2,18 @@ package application_error
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
 
 	"github.com/go-playground/validator/v10"
 	"justcallmesu.com/rest-api/internal/api/response"
 )
 
-func FormatValidationError(passedError error) []response.APIValidationError {
+func FormatValidationError(passedError error) any {
 	var validationErrors validator.ValidationErrors
+	var numError *strconv.NumError
 
 	if errors.As(passedError, &validationErrors) {
-
 		var formattedErrors = make([]response.APIValidationError, len(validationErrors))
 
 		for index, err := range validationErrors {
@@ -21,8 +23,12 @@ func FormatValidationError(passedError error) []response.APIValidationError {
 			}
 		}
 		return formattedErrors
+	} else if errors.As(passedError, &numError) {
+		return fmt.Sprintf(
+			"Nilai tidak valid: '%s'. Harap periksa kembali input Anda.",
+			numError.Num,
+		)
 	}
-
 	return nil
 }
 
@@ -39,6 +45,10 @@ func getErrorMessage(err validator.FieldError) string {
 		return "Field " + err.Field() + " must be at most " + err.Param() + " characters long"
 	case "len":
 		return "Field " + err.Field() + " must be exactly " + err.Param() + " characters long"
+	case "number", "numeric":
+		return "Field " + err.Field() + " must be a number"
+	case "oneof":
+		return "Field " + err.Field() + " must be one of the following: " + err.Param()
 	default:
 		return "Field " + err.Field() + " is invalid"
 	}
