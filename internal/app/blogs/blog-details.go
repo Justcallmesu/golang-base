a8124b731a05ddf2911a2d28db3a98932280ddb6
CodeNoobies <dev.justcallmesu@gmail.com>
2025-10-29 15:46:46 +0700

    feat(blogs): add BlogDetails type with attributes and constants

---
+++ internal/app/blogs/blog-details.go
@@ -0,0 +1,22 @@
+package blogs
+
+import (
+	"justcallmesu.com/rest-api/pkg/types"
+)
+
+type BlogDetailsType string
+
+const (
+	Image = "Image"
+	Code  = "Code"
+	Text  = "Text"
+)
+
+type BlogDetails struct {
+	types.BaseEntityModel
+	BlogId  uint            `json:"blogId"`
+	Blog    Blog            `json:"blog,omitzero"`
+	Content string          `json:"content"`
+	Url     string          `json:"url,omitempty"`
+	Type    BlogDetailsType `json:"type"`
+}
