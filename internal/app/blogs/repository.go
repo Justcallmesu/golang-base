353d2e8b1744572ba065a754f27223246ebda723
CodeNoobies <dev.justcallmesu@gmail.com>
2025-10-29 15:47:30 +0700

    refactor(repository): streamline FindMany, FindOne, UpdateOne, and DeleteOne methods for improved clarity and error handling

--- internal/app/blogs/repository.go
+++ internal/app/blogs/repository.go
@@ -20,7 +20,9 @@ func NewBlogRepository(db *gorm.DB) *BlogRepository {
 
 func (repository *BlogRepository) FindMany(parameter *BlogQuery, context context.Context) (*[]Blog, error) {
 
-	queryBuilder := gorm.G[Blog](repository.sqlDatabaseConnection)
+	var blogs []Blog
+
+	queryBuilder := repository.sqlDatabaseConnection.WithContext(context)
 
 	if parameter.Search != "" {
 		searchValue := "%" + parameter.Search + "%"
@@ -34,19 +36,22 @@ func (repository *BlogRepository) FindMany(parameter *BlogQuery, context context
 
 	queryBuilder.Limit(parameter.Limit).Offset(parameter.Limit * parameter.Page)
 
-	blogs, fetchError := queryBuilder.Find(context)
+	fetchError := queryBuilder.Find(&blogs).Error
 
 	return &blogs, fetchError
 }
 
 func (repository *BlogRepository) FindOne(targetId int, context context.Context) (Blog, error) {
-	foundBlog, fetchOneError := gorm.G[Blog](repository.sqlDatabaseConnection).Where("id = ?", targetId).Take(context)
 
-	if fetchOneError != nil {
-		return Blog{}, repository.HandleDatabaseError(fetchOneError)
+	var blog Blog
+
+	fetchError := repository.sqlDatabaseConnection.WithContext(context).Preload("Details").Preload("Details").First(&blog, targetId).Error
+
+	if fetchError != nil {
+		return Blog{}, repository.HandleDatabaseError(fetchError)
 	}
 
-	return foundBlog, (repository.HandleDatabaseError(fetchOneError))
+	return blog, (repository.HandleDatabaseError(fetchError))
 }
 
 func (repository *BlogRepository) CreateOne(newBlog *Blog, context context.Context) error {
@@ -56,13 +61,46 @@ func (repository *BlogRepository) CreateOne(newBlog *Blog, context context.Conte
 	return createError
 }
 
-func (repository *BlogRepository) UpdateOne(updatedBlog *Blog, context context.Context) (int, error) {
-	return gorm.G[Blog](repository.sqlDatabaseConnection).Where("id = ?", updatedBlog.ID).Updates(context, *updatedBlog)
+func (repository *BlogRepository) UpdateOne(updatedBlog *Blog, context context.Context) error {
+	transaction := repository.sqlDatabaseConnection.WithContext(context).Begin()
+
+	if transaction.Error != nil {
+		return repository.HandleDatabaseError(transaction.Error)
+	}
+
+	defer func() {
+		if transactionError := recover(); transactionError != nil {
+			transaction.Rollback()
+		}
+	}()
+
+	//  Update Parent Data
+	if updateError := transaction.Model(updatedBlog).Omit("Details").Updates(updatedBlog).Error; updateError != nil {
+		return repository.HandleDatabaseError(updateError)
+	}
+
+	// Update and Delete Association
+	if updateDetailsError := transaction.Unscoped().Model(updatedBlog).Association("Details").Unscoped().Replace(updatedBlog.Details); updateDetailsError != nil {
+		return repository.HandleDatabaseError(updateDetailsError)
+	}
+
+	return repository.HandleDatabaseError(transaction.Commit().Error)
 }
 
-func (repository *BlogRepository) DeleteOne(targetId int, context context.Context) (int, error) {
-	return gorm.G[Blog](repository.sqlDatabaseConnection).Where("id = ?", targetId).Delete(context)
+func (repository *BlogRepository) DeleteOne(targetId int, context context.Context) error {
+	transaction := repository.sqlDatabaseConnection.WithContext(context).Begin().Unscoped()
 
+	defer func() {
+		if transactionError := recover(); transactionError != nil {
+			transaction.Rollback()
+		}
+	}()
+
+	if deleteError := transaction.Where("id = ?", targetId).Delete(&Blog{}).Error; deleteError != nil {
+		return repository.HandleDatabaseError(deleteError)
+	}
+
+	return repository.HandleDatabaseError(transaction.Commit().Error)
 }
 
 func (repositorry *BlogRepository) HandleDatabaseError(databaseError error) error {
