9096dc31556adbbeb386142dc09463a34a8be2ed
CodeNoobies <dev.justcallmesu@gmail.com>
2025-11-07 13:19:56 +0700

    feat(app): integrate Files service and repository into app structure

--- internal/app/app.go
+++ internal/app/app.go
@@ -6,6 +6,7 @@ import (
 
 	"gorm.io/gorm"
 	"justcallmesu.com/rest-api/internal/api/middleware"
+	files "justcallmesu.com/rest-api/internal/app/Files"
 	"justcallmesu.com/rest-api/internal/app/auth"
 	"justcallmesu.com/rest-api/internal/app/blogs"
 	"justcallmesu.com/rest-api/internal/app/cookies"
@@ -21,11 +22,13 @@ type Services struct {
 	JWTService    *auth.JWTService
 	BlogService   *blogs.BlogService
 	SystemService *system.SystemService
+	FileService   *files.FilesService
 }
 
 type Repositories struct {
 	UserRepository *users.UserRepository
 	BlogRepository *blogs.BlogRepository
+	FileRepository *files.FilesRepository
 }
 
 type Middlewares struct {
@@ -35,9 +38,11 @@ type Middlewares struct {
 func NewRepositories(database *gorm.DB) *Repositories {
 	userRepository := users.NewUserRepository(database)
 	blogRepository := blogs.NewBlogRepository(database)
+	fileRepository := files.NewFilesRepository(database)
 	return &Repositories{
 		UserRepository: userRepository,
 		BlogRepository: blogRepository,
+		FileRepository: fileRepository,
 	}
 }
 
@@ -54,6 +59,7 @@ func NewServices(Repositories *Repositories) *Services {
 	imageService := images.NewImageUploadService(DEFAULT_WRITE_PATH, systemService)
 	authService := auth.NewAuthService(Repositories.UserRepository, jwtService, cookieService)
 	blogService := blogs.NewBlogService(Repositories.BlogRepository, imageService)
+	fileService := files.NewFilesService(Repositories.FileRepository)
 
 	return &Services{
 		CookieService: cookieService,
@@ -61,6 +67,7 @@ func NewServices(Repositories *Repositories) *Services {
 		JWTService:    jwtService,
 		BlogService:   blogService,
 		SystemService: systemService,
+		FileService:   fileService,
 	}
 }
 
