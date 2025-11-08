7c53e7dbdd0f70fe8d52acbe75cf2a40f9780736
CodeNoobies <dev.justcallmesu@gmail.com>
2025-11-08 11:04:07 +0700

    refactor(routes): update SetupRoutes to use dynamic upload path and clean up code

--- internal/api/routes/routes.go
+++ internal/api/routes/routes.go
@@ -1,17 +1,25 @@
 package routes
 
 import (
+	"os"
+	"path/filepath"
+
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 	"justcallmesu.com/rest-api/internal/app"
+	"justcallmesu.com/rest-api/internal/utils"
 )
 
 func SetupRoutes(ginEngine *gin.Engine, database *gorm.DB) {
 
+	var defaultWritePath = filepath.Clean(utils.GetDefaultValue(os.Getenv("UPLOAD_PATH"), "public/"))
+
 	var repositories = app.NewRepositories(database)
-	var services = app.NewServices(repositories)
+	var services = app.NewServices(repositories, defaultWritePath)
 
 	var middlewares = app.NewMiddlewares(services)
+
+	ginEngine.Static("/public", filepath.Clean(defaultWritePath))
 
 	AuthRoutes(ginEngine, services, middlewares)
 	ginEngine.Use(middlewares.AuthMiddleware.EnsureSessionIsValid())
