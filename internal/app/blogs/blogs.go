c702aadc982531ce2fbf46341e82758b794f32df
CodeNoobies <dev.justcallmesu@gmail.com>
2025-11-17 21:34:29 +0700

    fix(blog): add ViewCount field to Blog struct for tracking view statistics

--- internal/app/blogs/blogs.go
+++ internal/app/blogs/blogs.go
@@ -13,6 +13,7 @@ type Blog struct {
 	Title       string        `json:"title,omitempty" binding:"required"`
 	Description string        `json:"description,omitempty"  binding:"required"`
 	ReadingTime int32         `json:"readingTime,omitempty" binding:"required,number,gte=1"`
+	ViewCount   uint64        `json:"viewCount"`
 	Details     []BlogDetails `json:"details,omitzero" gorm:"foreignKey:BlogId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;references:Id"`
 }
 
