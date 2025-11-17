2df9032614d285902809f30420c972fce88c9a07
CodeNoobies <dev.justcallmesu@gmail.com>
2025-11-17 21:33:35 +0700

    fix(blog): improve error handling in BlogDetailsRepository for database operations

--- internal/app/blogs/blog-details-repository.go
+++ internal/app/blogs/blog-details-repository.go
@@ -2,6 +2,7 @@ package blogs
 
 import (
 	"context"
+	"errors"
 
 	"gorm.io/gorm"
 )
@@ -21,4 +22,52 @@ func (repository BlogDetailsRepository) CreateOne(blogDetails *BlogDetails, cont
 	createError := gorm.G[BlogDetails](repository.database, result).Create(context, blogDetails)
 
 	return createError
+}
+
+func (repository *BlogDetailsRepository) UpdateOne(blogDetails *BlogDetails, context context.Context) error {
+	updateError := repository.database.Transaction(func(transaction *gorm.DB) error {
+
+		updateError := transaction.Model(blogDetails).Updates(blogDetails).Error
+
+		if updateError != nil {
+			return updateError
+		}
+
+		return nil
+
+	})
+
+	if updateError != nil {
+		return repository.HandleDatabaseError(updateError)
+	}
+
+	return nil
+}
+
+func (repository *BlogDetailsRepository) UpdateDetailOrder(updateBlogDetailsOrder *[]UpdateBlogDetailsOrder) error {
+	updateError := repository.database.Transaction(func(tx *gorm.DB) error {
+		for _, item := range *updateBlogDetailsOrder {
+			if err := tx.Model(&BlogDetails{}).Where("id = ?", item.Id).UpdateColumn("order", item.Order).Error; err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+
+	if updateError != nil {
+		return repository.HandleDatabaseError(updateError)
+	}
+
+	return nil
+}
+
+func (repository BlogDetailsRepository) HandleDatabaseError(databaseError error) error {
+	if databaseError == nil {
+		return nil
+	}
+
+	if errors.Is(databaseError, gorm.ErrRecordNotFound) {
+		return errors.New("detail blog tidak ditemukan")
+	}
+	return errors.New("[Blog Detail] Internal Server Error")
 }
