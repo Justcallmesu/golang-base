b78afd6bce5cfc1845521aac3adc5f081dbe5ba4
CodeNoobies <dev.justcallmesu@gmail.com>
2026-01-08 11:33:29 +0700

    refactor(database): streamline database connection handling and improve DSN generation

--- internal/database/service.go
+++ internal/database/service.go
@@ -1,35 +1,84 @@
 package database
 
 import (
-	"database/sql"
 	"fmt"
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"justcallmesu.com/golang-base/internal/utils/default_value"
 )
 
-var Database *sql.DB
-
 func InitConnection() *gorm.DB {
-	datasourceNetwork := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_NAME"),
-	)
+	databaseType := DatabaseType(default_value.GetDefaultValue(os.Getenv("DB_TYPE"), "mysql"))
 
-	database, databaseError := gorm.Open(mysql.New(mysql.Config{
-		DSN: datasourceNetwork,
-	}))
+	var database *gorm.DB
 
-	if databaseError != nil {
-		// This error is rare but should be handled.
-		panic(fmt.Sprintf("Database connection error: %v", databaseError))
-
+	switch databaseType {
+	case MySQL:
+		database = ConnectSQL()
+	case Postgres:
+		database = ConnectPostgres()
+	default:
+		panic("Unsupported database type, supported types are: mysql, postgres")
 	}
 
 	return database
+}
+
+func ConnectSQL() *gorm.DB {
+	dsn := GetDatabaseDSN()
+
+	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic(fmt.Sprintf("SQL Database connection failed: %v", err))
+	}
+
+	return database
+}
+
+func ConnectPostgres() *gorm.DB {
+	dsn := GetDatabaseDSN()
+
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic(fmt.Sprintf("Postgres Database connection failed: %v", err))
+	}
+
+	return database
+}
+
+func GetDatabaseDSN() string {
+	databaseUser := default_value.GetDefaultValue(os.Getenv("DB_USER"), "root")
+	databasePassword := default_value.GetDefaultValue(os.Getenv("DB_PASSWORD"), "")
+	databaseHost := default_value.GetDefaultValue(os.Getenv("DB_HOST"), "localhost")
+	databasePort := default_value.GetDefaultValue(os.Getenv("DB_PORT"), "3306")
+	databaseName := default_value.GetDefaultValue(os.Getenv("DB_NAME"), "golang_base")
+
+	var databaseType = DatabaseType(default_value.GetDefaultValue(os.Getenv("DB_TYPE"), "mysql"))
+
+	switch databaseType {
+	case MySQL:
+		return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
+			databaseUser,
+			databasePassword,
+			databaseHost,
+			databasePort,
+			databaseName,
+		)
+	case Postgres:
+		return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+			databaseHost,
+			databasePort,
+			databaseUser,
+			databasePassword,
+			databaseName,
+		)
+	default:
+		panic("Unsupported database type, supported types are: mysql, postgres")
+	}
 }
